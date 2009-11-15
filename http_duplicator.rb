3d488ae4fc0a097ed1193a4e5e5a0fb19f3b4e06
Renaud Chaput <renchap@cocoa-x.com>
2009-11-15 18:04:19 +0100

    Second version, more complete.
    Uses more libraries, works fine for GET.
    
    Problem with HEAD : does not returns to the client (callback not called
    ?)
    Problem with PUT : no content in file

--- http_duplicator.rb
+++ http_duplicator.rb
@@ -1,26 +1,90 @@
 require 'rubygems'
 require 'eventmachine'
-require 'socket'
+require 'evma_httpserver'
+require 'event_utils'
+require 'json/add/core'
 
-module HTTP_Duplicator
-  def receive_data data
-    backends = [
-      { :host => 'localhost', :port => 10001 },
-      { :host => 'localhost', :port => 10002 },
-    ]
-    p data
-    puts "Sending to backends..."
-    backends.each do |backend|
-      puts "Backend #{backend[:host]}:#{backend[:port]}..."
-      s = TCPSocket.open(backend[:host], backend[:port])
-      s.puts data
-      s.close
-      puts "Done"
-    end
-    puts "All done"
+class Backend
+  attr_reader :host, :port, :status, :headers, :content
+
+  include EM::Deferrable
+
+  def initialize(host, port)
+    @host = host
+    @port = port
+  end
+
+  def send_request(params)
+     client = EventMachine::Protocols::HttpClient.request(params)
+
+     client.callback {|response|
+       @status = response[:status]
+       @headers = response[:headers]
+       @content = response[:content]
+       self.succeed
+     }
+     client.errback { self.fail }
+     return self
   end
 end
 
-EventMachine::run do
-  EventMachine::start_server '127.0.0.1', 10000, HTTP_Duplicator
+class HTTP_Duplicator < EM::Connection
+  include EM::HttpServer
+  include EventUtils
+  
+  def post_init
+    super
+    no_environment_strings
+  end
+
+  def process_http_request
+    backends = [
+      Backend.new('localhost', 10001),
+      Backend.new('localhost', 10002)
+    ]
+
+    in_deferred_loop do
+      results = Array.new
+
+      backends.each do |backend|
+        results << backend.send_request(
+          :host => backend.host,
+          :port => backend.port,
+          :request => @http_request_uri,
+          :verb => @http_request_method,
+          :content => @http_request_post_content,
+          :contenttype => @http_content_type,
+          :querystring => @http_query_string
+        )
+      end
+
+      waiting_for(*results) do
+        puts "Got all answers !"
+        status_codes = Hash.new
+        server_status = Hash.new
+
+        results.each do |result|
+          puts "* #{result.host}:#{result.port} - HTTP #{result.status}"
+          server_status[result.host+':'+result.port.to_s] = result.status
+          status_codes[result.status] = 0 unless status_codes[result.status]
+          status_codes[result.status] += 1
+        end
+
+        # Send back response
+        response = EM::DelegatedHttpResponse.new(self)
+        if status_codes[200] and status_codes[200].size == results.size
+          response.status = 200
+        else
+          response.status = 503
+        end
+        response.content = server_status.to_json
+        response.content_type 'application/json'
+        response.send_response
+      end
+    end
+  end
+end
+
+EM::run do
+  EM::start_server '127.0.0.1', 10000, HTTP_Duplicator
 end
